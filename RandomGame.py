318e1d1703a74f82cc7521ebfb750b073e2dbdf7
Nabatov_Evgeniy <NabatovLearn@Yandex.ru>
2023-07-10 15:29:58 +0300

    Initial commit

---
+++ RandomGame.py
@@ -0,0 +1,12 @@
+import random
+
+FirstPlayerHand = []
+# TODO SecondPlayerHand
+# TODO Board
+# TODO Players stack with cards
+
+for i in range(0, 5):
+    n = random.randint(1, 10)
+    FirstPlayerHand.append(n)
+print(FirstPlayerHand)
+
