9847abca172fa2da42fae09da5de60f526e0a882
Nabatov_Evgeniy <NabatovLearn@Yandex.ru>
2023-07-10 20:14:17 +0300

    ошибка для проверки работы pylint

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 import xlsxwriter
 
 
-workbook = xlsxwriter.Workbook('training_data/OMS.xlsx')
-worksheet = workbook.add_worksheet()
+# workbook = xlsxwriter.Workbook('training_data/OMS.xlsx')
+# worksheet = workbook.add_worksheet()
 
 
 workbook.close()
